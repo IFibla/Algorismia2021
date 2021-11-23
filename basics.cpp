601a0fda970ac0c0d095670a3931d9d43c8aa1af
Jaya Gracias <jayonga3@gmail.com>
2021-11-23 17:13:04 +0100

    Primer apartat fet (amb 3 exemples d'entrada)

--- basics.cpp
+++ basics.cpp
@@ -7,9 +7,37 @@ using namespace std;
 using Adjacencia = set<int>;
 using Graf = vector<Adjacencia>;
 
-bool isMinimalPositiveInfluenceDominatingSet(const Graf& G, const set<int>& D, vector<bool>& vistat) {
+bool isMinimalPositiveInfluenceDominatingSet(const Graf& G, const set<int>& D, vector<bool>& visitat) {
 
-    bool esMPIDS = true;
+    bool esMPIDS = false;
+
+    std::set<int>::iterator it = D.begin();
+
+    while (it != D.end() and not esMPIDS) {                                         // per cada vertex u de D mirem que passa si el treiem
+
+        set<int> Adjacents = G[*it];                                            // adjacencies del vertex u
+
+        for (int aresta : Adjacents) {                                          // per tota adjacencia v de u
+
+            int nAdjacenciesD = 0;
+
+            for (int a : G[aresta]) {                                           // mirem les adjacencies d'v tret el vertex u
+
+                std::set<int>::iterator i = D.find(a);
+                if (i != D.end() and *i != *it) ++nAdjacenciesD;
+
+            }
+
+            int nAdjacenciesTotals = G[aresta].size() -1;
+
+            float proporcioAdjaD = float(nAdjacenciesD)/float(nAdjacenciesTotals);
+            if (proporcioAdjaD < 0.5) esMPIDS = true;
+
+        }
+
+        ++it;
+
+    }
 
     return esMPIDS;
 }
@@ -42,7 +70,7 @@ bool isPositiveInfluenceDominatingSet(const Graf& G, const set<int>& D, vector<b
 
             int nAdjacenciesTotals = G[v].size();
 
-            double proporcioAdjaD = double(nAdjacenciesD)/double(nAdjacenciesTotals);
+            float proporcioAdjaD = float(nAdjacenciesD)/float(nAdjacenciesTotals);
             if (proporcioAdjaD < 0.5) esPIDS = false;
 
         }
@@ -81,11 +109,22 @@ int main() {
 
     vector<bool> visitat(n, false);
 
+    cout << endl;
+
     cout << "Comprovant si D es PIDS..." << endl;
 
-    if (isPositiveInfluenceDominatingSet(G, D, visitat))
-        cout << "El conjunt es d'influencia positiva" << endl;
+    cout << endl;
 
+    if (isPositiveInfluenceDominatingSet(G, D, visitat)) {
+        cout << "El conjunt es d'influencia positiva" << endl;
+        cout << endl;
+        cout << "Comprovant si D es minimal..." << endl;
+        cout << endl;
+        if (isMinimalPositiveInfluenceDominatingSet(G, D, visitat))
+            cout << "El conjunt es d'influencia positiva i es minimal" << endl;
+        else cout << "El conjunt es d'influencia positiva pero no es minimal" << endl;
+            
+    }
     else cout << "El conjunt no es d'influencia positiva" << endl;
 
 }
