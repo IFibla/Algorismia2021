55a944f07141f0ff851f3137a25bee26d9ade623
Altariii <albert.tarres.ribalta@estudiantat.upc.edu>
2021-11-26 13:34:59 +0100

    localSearch (Bad Swap)

--- SOURCES/local_search.cpp
+++ SOURCES/local_search.cpp
@@ -92,65 +92,207 @@ void read_parameters(int argc, char **argv) {
     }
 }
 
+void MergeSortedIntervals(vector<set<int> >& v, int s, int m, int e) {
+
+    // temp is used to temporary store the vector obtained by merging
+    // elements from [s to m] and [m+1 to e] in v
+    vector<set<int> > temp;
+
+    int i, j;
+    i = s;
+    j = m + 1;
+
+    while (i <= m && j <= e) {
+
+        if (v[i].size() <= v[j].size()) {
+            temp.push_back(v[i]);
+            ++i;
+        }
+        else {
+            temp.push_back(v[j]);
+            ++j;
+        }
+
+    }
+
+    while (i <= m) {
+        temp.push_back(v[i]);
+        ++i;
+    }
+
+    while (j <= e) {
+        temp.push_back(v[j]);
+        ++j;
+    }
+
+    for (int i = s; i <= e; ++i)
+        v[i] = temp[i - s];
+
+}
+
+// the MergeSort function
+// Sorts the array in the range [s to e] in v using
+// merge sort algorithm
+void MergeSort(vector<set<int> >& v, int s, int e) {
+    if (s < e) {
+        int m = (s + e) / 2;
+        MergeSort(v, s, m);
+        MergeSort(v, m + 1, e);
+        MergeSortedIntervals(v, s, m, e);
+    }
+}
+
+int computeH(const set<int>& v, set<int>& S) {
+
+    int compt = 0;
+    for (int a : v) {                                   // per tot vei de a
+        std::set<int>::iterator it = S.find(a);
+        if (it != S.end()) ++compt;                     // Veins de v a S
+    }
+
+    int n = ceil(float(v.size())/2);                    // upper bound de grau(v)/2
+    return n-compt;
+}
+
+int cover_degree(const vector<set<int> >& G, set<int>& S, set<int>& SC, int i) {
+
+    int argMax = 0;
+    int aux = 0;
+    int nCoverMax = -1;
+
+    for (int a : G[i]) {
+
+        std::set<int>::iterator it = SC.find(a);
+        int covered = 0;
+        if (it != SC.end()) {
+            for (int aresta : G[a]) {
+                if (computeH(G[aresta], S) > 0) {
+                    ++covered;
+                    aux = aresta;
+                }
+            }
+
+            if (covered > nCoverMax) {
+                nCoverMax = covered;
+                argMax = a;
+            }
+        }
+    }
+    return argMax;
+}
+
+void greedy(const vector<set<int> >& G, set<int>& S) {
+
+    int n = G.size();
+    set<int> SC;
+    for (int i = 0; i < n; ++i) {
+        SC.insert(i);
+    }
+
+    for (int i = 0; i < n; ++i) {
+
+        int p = computeH(G[i], S);                  // mirem si el vertex i esta cobert
+        if (p > 0) {
+        
+            for (int j = 0; j < p; ++j) {
+                int argmax = cover_degree(G, S, SC, i);             // cover degree dels vertexs adjacents a i que estan en SC
+                S.insert(argmax);
+                SC.erase(argmax);
+            }
+        }
+    }
+}
+
 double getHeuristic(const set<int> &solution, const vector<set<int>> &neighbors) {
     double heuristic = 0;
-    int n = solution.size();
-    for (int i = 0; i < n; ++i) {
-        int needed_neighbors = round(neighbors[i].size() / 2.0);
-        int counted_neighbors = 0;
-        for (set<int>::iterator it = neighbors[i].begin(); it != neighbors[i].end(); it++) {
-            if (solution.find(*it) != solution.end()) counted_neighbors++;
-        }
-        if ((counted_neighbors - needed_neighbors) == 0) {
-            for (int k : solution) {
-                heuristic += neighbors[k].size();
+    for(int i = 0; i < neighbors.size(); ++i) {
+        if (solution.find(i) == solution.end()) {
+             for (set<int>::iterator it = neighbors[i].begin(); it != neighbors[i].end(); ++it) {
+                if (solution.find(*it) == solution.end()) heuristic += 1;
             }
-        }else if ((counted_neighbors - needed_neighbors) > 0) {
-            heuristic -= counted_neighbors - needed_neighbors;
         }
-        heuristic += (counted_neighbors - needed_neighbors) + 1;
     }
     return heuristic;
 }
 
-bool is_Solution(const set<int> &solution, const vector<set<int>> &neighbours) {
-    for (int i = 0; i < neighbors.size(); i++) {
-        int needed_neighbors = round(neighbors[i].size() / 2.0);
+bool is_Solution(const set<int> &solution, const vector<set<int>> &neighbours, const int &j) {
+    //cout << "Deleted from solution vertex " << j << endl;
+    for (int i : neighbours[j]) {
+        int needed_neighbors = round(neighbours[i].size() / 2.0);
+        //cout << "Seeing if neighbor " << i << " has enough vertex. (Needed: " << neighbours[i].size() / 2.0 << ")." << endl;
+        //cout << i << "'s first neighbor is " << *(neighbours[i].begin()) << endl;
         int counted_neighbors = 0;
-        for (set<int>::iterator it = neighbors[i].begin(); it != neighbors[i].end(); it++) {
-            if (solution.find(*it) != solution.end()) counted_neighbors++;
+        for (set<int>::iterator it = neighbours[i].begin(); it != neighbours[i].end(); it++) {
+            if (solution.find(*it) != solution.end()) {
+                //cout << "FOUND A NEIGHBOR: " << *it << endl;
+                counted_neighbors++;
+            }
         }
+        //cout << "Found " << counted_neighbors << " counted neighbors." << endl << endl;
         if (counted_neighbors < needed_neighbors) return false;
     }
     return true;
 }
 
-vector<set<int>> generateSuccessors(set<int> &actual, vector< set<int>>& neighbors) {
-   vector<set<int>> successors;
-   for (int i = 0; i < neighbors.size(); i++) {
-       set<int> aux = actual;
-       if (actual.find(i) == actual.end()) {
-            aux.insert(i);
-            successors.push_back(aux);
-       }
-   }
-   return successors;
+
+void swap(set<int>& aux,int i, int j) {
+    aux.erase(i);
+    aux.insert(j);
 }
 
+bool eliminateVertex(set<int> &actual, vector< set<int>>& neighbors, set<int>& newsol, set<int>::iterator& last) {
+    vector<set<int>> successors;
+    for (set<int>::iterator it = last; it != actual.end(); ++it) {
+       set<int> aux = actual;
+       aux.erase(*it);
+       if (is_Solution(aux, neighbors, *it )) {
+           newsol = aux;
+           last = it;
+           return true;
+       }
+    }
+    return false;
+}
 
+bool nextSwap(set<int> &actual, vector< set<int>>& neighbors, set<int>& newsol) {
 
-set<int> getBetterSon(vector<set<int>> successors, const vector<set<int>> &neighbours) {
-    set<int> better_son = successors[0];
-    int better_heuristic = getHeuristic(better_son, neighbors);
-    int N = successors.size();
-    for (int i = 1; i < N; i++) {
-        int new_heuristic = getHeuristic(successors[i], neighbors);
-        if (new_heuristic > better_heuristic) {
-            better_son = successors[i];
-            better_heuristic = new_heuristic;
+    double heurActual = getHeuristic(actual, neighbors);
+    double heurNewSol;
+    pair<int, int> bestSwap;
+    bool primer = true;
+    int n = neighbors.size();
+    for (set<int>::iterator it = actual.begin(); it != actual.end(); ++it) {
+        for (int i = 0; i < n; ++i) {
+            set<int> aux = actual;
+            if (actual.find(i) == actual.end()) {
+                swap(aux, *it,  i);
+                if (is_Solution(aux, neighbors, *it )) {
+                    if (primer) {
+                        primer = false;
+                        bestSwap.first = *it;
+                        bestSwap.second = i;
+                        heurNewSol = getHeuristic(aux, neighbors);
+                    } else {
+                        double heurAux = getHeuristic(aux, neighbors);
+                        if ( heurAux > heurNewSol) {
+                            bestSwap.first = *it;
+                            bestSwap.second = i;
+                            heurNewSol = heurAux;
+                        }
+                    }
+                }
+            }
         }
     }
-    return better_son;
+
+    if (heurNewSol > heurActual) {
+        swap(actual, bestSwap.first, bestSwap.second);
+        newsol = actual;
+        return true;
+    }else {
+        return false;
+    }
+    
 }
 
 /**********
@@ -164,7 +306,7 @@ int main( int argc, char **argv ) {
     // setting the output format for doubles to 2 decimals after the comma
     std::cout << std::setprecision(2) << std::fixed;
 
-    // initializing the random number generator. 
+    // initializing the random number generator.
     // A random number in (0,1) is obtained with: double rnum = rnd->next();
     rnd = new Random((unsigned) time(&t));
     rnd->next();
@@ -201,51 +343,61 @@ int main( int argc, char **argv ) {
         // double ct = timer.elapsed_time(Timer::VIRTUAL);
 
         cout << "start application " << na + 1 << endl;
+        
 
         // Solució inicial (tots els vertex)
 
-        set<int> actual;
+        set<int> S;                 // S will contain the final solution
+        vector<set<int>> initial_neighbors = neighbors;
+
+        MergeSort(neighbors, 0, neighbors.size()-1);            // ordenem primer els vertexs respecte el seu grau de manera ascendent
+        greedy(neighbors, S);
+
+        set<int> actual = S;
 
         bool end = false;
 
         /*
-        cout << "First solution: " << endl;
-        for (int i : actual) cout << i << " ";
-        cout << endl;
+        for(int i = 0; i < initial_neighbors.size(); ++i) {
+            cout << "Vertex :" << i << "  Veins: ";
+            for (set<int>::iterator it = initial_neighbors[i].begin(); it != initial_neighbors[i].end(); ++it) {
+                cout << *it << " ";
+            }
+            cout << endl;
+        }
         */
+
+       set<int>::iterator last = actual.begin();
         
        cout << actual.size() << endl;
 
+       set<int> solaux;
+
         while (not end) {
 
-            // for (int i : actual) cout << i << " ";
-            vector<set<int>> succesors = generateSuccessors(actual, neighbors);
-            
-            /*
-            cout << "SUCCESSORS: " << endl;
-            for (set<int> successor : succesors) {
-                for (int s : successor) cout << s << " ";
-                cout << endl;
-            } 
-            */
-           
-            actual = getBetterSon(succesors, neighbors);
-
-            /*
-            cout << "New solution: " << endl;
-            for (int i : actual) cout << i << " ";
-            cout << endl;
-            */
-            
-            if (is_Solution(actual, neighbors)) end = true;
+            if (eliminateVertex(actual, initial_neighbors, solaux, last)) {
+                actual = solaux;
+            } else {
+                if (nextSwap(actual, initial_neighbors, solaux)) {
+                    actual = solaux;
+                } else {
+                    end = true;
+                }
+                last = actual.begin();
+            }
+            //cout << actual.size() << endl;
         }
 
         cout << actual.size() << endl;
 
+        
+        /*
         for (set<int>::iterator it = actual.begin(); it != actual.end(); ++it) { 
             cout << *it << " ";
         }
         cout << endl;
+        */
+        
         // HERE GOES YOUR LOCAL SEARCH METHOD
 
         // The starting solution for local search may be randomly generated, 
